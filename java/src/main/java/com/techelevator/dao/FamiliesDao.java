cb909e6b92122f075afce33b7f631c473a97e0af
James Malloy <malloyj08@gmail.com>
2021-12-07 15:03:15 -0500

    Pushing family.java, families.dao, jdbcfamiliesdao

---
+++ java/src/main/java/com/techelevator/dao/FamiliesDao.java
@@ -0,0 +1,8 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Family;
+
+public interface FamiliesDao {
+
+    Family getFamilyByName(String familyName);
+}
--- java/src/main/java/com/techelevator/model/Family.java
+++ java/src/main/java/com/techelevator/model/Family.java
@@ -5,6 +5,8 @@ public class Family {
     int familyId;
     String familyName;
 
+    public Family() {}
+
     public Family(int familyId, String familyName) {
         this.familyId = familyId;
         this.familyName = familyName;
@@ -16,5 +18,13 @@ public class Family {
 
     public String getFamilyName() {
         return familyName;
+    }
+
+    public void setFamilyId(int familyId) {
+        this.familyId = familyId;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
     }
 }
