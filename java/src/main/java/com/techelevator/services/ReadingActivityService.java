2284656263be417f417f7275d191bbb400c47245
Akib Choudhury <akibchoudhury111@gmail.com>
2021-12-08 10:27:34 -0500

    addition

---
+++ java/src/main/java/com/techelevator/services/ReadingActivityService.java
@@ -0,0 +1,17 @@
+package com.techelevator.services;
+
+import com.techelevator.model.User;
+import org.springframework.web.client.RestTemplate;
+
+public class ReadingActivityService {
+
+    private static String API_BASE_URL;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private User currentUser;
+
+
+
+
+}
