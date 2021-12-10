17ff3535db84c39edf5dd4cb5d0c4d84edba5ee6
Clara <clarabrook97@gmail.com>
2021-12-10 11:37:50 -0500

    jdbcreaderdao

--- java/src/main/java/com/techelevator/dao/JdbcReaderDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcReaderDao.java
@@ -4,16 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-<<<<<<< HEAD
 public class JdbcReaderDao implements ReaderDao {
-=======
-public class JdbcReaderDao {
->>>>>>> 9272a3bc3f7bc6ebd2f7f5adff0eb403eb084715
 
-<<<<<<< HEAD
-=======
-
->>>>>>> dd1d4d9637aac2923f4253bea448c4574fe9bfb3
     private JdbcTemplate jdbcTemplate;
 
     public JdbcReaderDao(JdbcTemplate jdbcTemplate) {
